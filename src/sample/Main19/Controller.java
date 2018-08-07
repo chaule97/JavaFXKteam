82c160a6e1314cf2f1f9295731a45117b4b039b5
Le Chau <lebaochau97@gmail.com>
2018-08-08 00:37:23 +0700

    'repair_Pie_Chart'

--- src/sample/Main19/Controller.java
+++ src/sample/Main19/Controller.java
@@ -4,7 +4,9 @@ package sample.Main19;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
     @FXML
@@ -22,6 +24,14 @@ public class Controller {
             PieChart.Data Python = new PieChart.Data("Python", PythonValue);
             pieChart.getData().clear();
             pieChart.getData().addAll(CSharp, Java, Python);
+            for (PieChart.Data data : pieChart.getData()){
+                data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Data");
+                    alert.setContentText(data.getName()+":"+data.getPieValue());
+                    alert.showAndWait();
+                });
+            }
         }catch (Exception ex){
 
         }
